39666572fc1e4a9e9c9c955eb9aa353fdb6e0a9b
Christopher Dent <christopherdent01@gmail.com>
2019-06-20 00:49:22 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,30 @@
 def addition(num1, num2)
-
+  puts (5 + 4)
+  return (num1 + num2)
 end
 
 def subtraction(num1, num2)
-
+  puts (10 - 5)
+  return (num1 - num2)
 end
 
 def division(num1, num2)
-
+  puts (50 / 2)
+  return (num1 / num2)
 end
 
 def multiplication(num1, num2)
-
+  puts (4 * 30)
+  return (num1 * num2)
 end
 
 def modulo(num1, num2)
+  puts (34 % 5)
+  return (num1 % num2)
 
 end
 
 def square_root(num)
-
+  puts Math.sqrt (81)
+  return Math.sqrt (num)
 end
